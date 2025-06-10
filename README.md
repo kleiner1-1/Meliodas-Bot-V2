7f242a21e0fd1975c725071b5a6a14e1492741f3
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-06-09 21:23:20 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,104 @@
+
+<h1 align="center">✰ Meliodas Bot MD ✰</h1>
+
+---
+
+<p align="center">
+  <img src="https://files.catbox.moe/hnif5j.jpg" alt="Kirito Bot MD Logo">
+</p>
+
+---
+
+# *Acerca de Meliodas Bot MD*
+
+> Un bot de WhatsApp potente y versátil, inspirado en el universo de Sword Art Online. Desarrollado para ofrecer una experiencia ágil, segura y funcional.
+
+*   *Desarrollador:* Deylin Eliac
+*   *Plataforma:* WhatsApp
+*   *Inspiración:* Sword Art Online
+*   *Características Clave:* Agilidad, Funcionalidad, Seguridad
+
+---
+
+## *Licencia*
+
+Este proyecto se distribuye bajo la [Licencia Pública General GNU Affero v3](./LICENSE).
+
+---
+
+<a
+href="https://www.mediafire.com/file/3hsvi3xkpq3a64o/termux_118.a"><img src="https://qu.ax/finc.jpg" height="125px"></a>
+
+# *Instalación en Termux*
+
+<details>
+  <summary><b>*Guía de Instalación Paso a Paso*</b></summary>
+
+  1.  `termux-setup-storage`
+  2.  `pkg update -y && pkg upgrade -y`
+  3.  `pkg install git nodejs ffmpeg imagemagick yarn -y`
+  4.  `git clone https://github.com/Deylin-Eliac/Kirito-Bot-MD`
+  5.  `cd Kirito-Bot-MD`
+  6.  `yarn install`
+  7.  `yarn start`
+
+  *Nota:* Los errores en rojo durante el escaneo del código QR son normales.
+
+  *Reiniciar el bot:*
+
+  ```bash
+  cd Meliodas-Bot-MD
+  yarn start
+  ```
+
+  *Escanear un nuevo código QR:*
+
+  ```bash
+  cd Meliodas-Bot-MD
+  rm -rf kiritoSession
+  yarn start
+  ```
+
+  *Mantener el bot activo en segundo plano:*
+
+  ```bash
+  npm i -g pm2
+  pm2 start index.js
+  pm2 save
+  pm2 logs
+  ```
+</details>
+
+---
+
+# *Comunidad*
+
+*   *Canal Oficial de WhatsApp:* [Únete aquí 👑]()
+
+---
+
+
+
+# *Estadísticas del Proyecto*
+
+<p align="center">
+  <img src="https://github-readme-stats.vercel.app/api?username=Deylin-Eliac&repo=Kirito-Bot-MD&show_icons=true&theme=radical&hide_border=true" alt="GitHub Stats">
+  <img src="https://github-readme-streak-stats.herokuapp.com/?user=Deylin-Eliac&theme=radical&hide_border=true" alt="GitHub Streak">
+</p>
+
+<p align="center">
+  <a href="https://github.com/Deylin-Eliac">
+    <img src="https://img.shields.io/badge/GitHub-Deylin-181717?style=for-the-badge&logo=github">
+  </a>
+  <a href="https://wa.me/50433191934">
+    <img src="https://img.shields.io/badge/WhatsApp-Contactar-25D366?style=for-the-badge&logo=whatsapp">
+  </a>
+  <a href="mailto:deylibaquedano801@gmail.com">
+    <img src="https://img.shields.io/badge/Email-Enviar%20correo-EA4335?style=for-the-badge&logo=gmail">
+  </a>
+</p>
+
+---
+
+--
+
---
+++ config.js
@@ -0,0 +1,114 @@
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone' 
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+
+global.botNumberCode = '' 
+global.confirmCode = ''
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.owner = [
+  ['573162402768', '🜲 Propietario 🜲', true],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  // reconocimiento por lid
+  [],
+  [],
+  ['111270153982054', 'lid', 'Bajo Bots', true],
+];
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.mods = ['573162402768']
+global.suittag = ['573162402768'] 
+global.prems = []
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.libreria = 'Baileys'
+global.baileys = '@whiskeysockets/baileys' 
+global.languaje = 'Español'
+global.vs = '2.0.19'
+global.vsJB = '6.5'
+global.nameqr = '𝕄𝔼𝕃𝕀𝕆𝔻𝔸𝕊 𝔹𝕆𝕋'
+global.namebot = '𝙼𝙴𝙻𝙸𝙾𝙳𝙰𝚂 𝙱𝙾𝚃'
+global.sessions = 'Sessions'
+global.jadi = 'JadiBots' 
+global.kiritoJadibts = true
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.packname = '𝙈𝙚𝙡𝙞𝙤𝙙𝙖𝙨 𝙗𝙤𝙩 𝐌𝐃'
+global.botname = '𝙈𝙚𝙡𝙞𝙤𝙙𝙖𝙨 - 𝑩𝒐𝒕'
+global.wm = '𝙈𝙚𝙡𝙞𝙤𝙙𝙖𝙨 𝐌𝐃'
+global.author = 'Made By Bajo'
+global.dev = '© ⍴᥆ᥕᥱrᥱძ ᑲᥡ Bajo'
+global.textbot = '𝙈𝙚𝙡𝙞𝙤𝙙𝙖𝙨 - 𝐁𝐨𝐭 • Powered By Bajo'
+global.etiqueta = 'Bajo'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.moneda = 'llamas'
+//global.welcom1 = '❍ Edita Con El Comando setwelcome'
+//global.welcom2 = '❍ Edita Con El Comando setbye'
+global.banner = 'https://qu.ax/rEJmN.jpg'
+global.avatar  = 'https://qu.ax/iZYvt.png'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.catalogo = fs.readFileSync('./src/catalogo.jpg');
+global.photoSity = [catalogo]
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.gp1 = ''
+global.comunidad1 = ''
+global.channel = ''
+global.channel2 = '
+global.md = ''
+global.correo = 'Kleinergalindo17@gmail.com'
+global.cn ='';
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.catalogo = fs.readFileSync('./src/catalogo.jpg');
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.ch = {
+ch1: '120363365444927738@newsletter',
+}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.MyApiRestBaseUrl = 'https://api.cafirexos.com';
+global.MyApiRestApikey = 'BrunoSobrino';
+global.openai_org_id = 'org-3';
+global.openai_key = 'sk-0';
+global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f'];
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())];
+global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63'];
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())];
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5'];
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())];
+global.lolkeysapi = ['kurumi']; // ['BrunoSobrino_2']
+global.itsrose = ['4b146102c4d500809da9d1ff'];
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
