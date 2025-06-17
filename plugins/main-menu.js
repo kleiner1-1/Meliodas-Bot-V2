997df772e497e7ec142d426cd0c98542884cdeb3
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-06-17 01:22:07 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -63,7 +63,7 @@ let handler = async (m, { conn, usedPrefix: _p}) => {
       //"🔊 Audios": new Set(),
      // "🗝️ Avanzado": new Set(),
       //"🔥 Free Fire": new Set(),
-      "grupos": new Set()
+      //"grupos": new Set()
       //"Otros": new Set() 
 };
 
