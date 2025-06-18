f4dbefdc576f7cb572b6b3aa6e5ca6ac669ac457
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-06-18 15:37:56 -0500

    Update group-demote.js

--- plugins/group-demote.js
+++ plugins/group-demote.js
@@ -6,8 +6,8 @@ var number = text.split`@`[1]
 }else if(!isNaN(text)) {
 var number = text
 }
-if(!text && !m.quoted) return conn.reply(m.chat, `🚩 Menciona a una persona.`, m, rcanal)
-if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `🚩 Menciona a una persona.`, m, rcanal)
+if(!text && !m.quoted) return conn.reply(m.chat, `🚩 Menciona a una persona.`, m, )
+if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `🚩 Menciona a una persona.`, m,)
 try {
 if(text) {
 var user = number + '@s.whatsapp.net'
