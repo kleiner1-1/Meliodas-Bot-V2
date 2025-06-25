c43e634115307f7beacc5a9c47a2f9b505bfbad8
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-06-25 18:34:20 -0500

    Update group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, isAdmin, isROwner }) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    await conn.reply(m.chat, `🧑‍💻BARBOZABOT FUE DESACTIVADO EN ESTE CHAT`, m, rcanal)
+    await conn.reply(m.chat, `🧑‍💻𝙈𝙀𝙇𝙄𝙊𝘿𝘼𝙎 𝘽𝙊𝙏 𝙁𝙐𝙀 𝘽𝘼𝙉𝙀𝘼𝘿𝙊 𝙀𝙉 𝙀𝙎𝙏𝙀 𝘾𝙃𝘼𝙏`, m, rcanal)
     await m.react('✅')
 }
 handler.help = ['banearbot']
