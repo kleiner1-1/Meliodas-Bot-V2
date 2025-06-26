38cbb38591a653b3d9e5f24edee216307c943d04
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-06-26 02:12:24 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -8,7 +8,7 @@ const clockString = ms => {
   return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':')
 }
 
-const imagen = "https://qu.ax/DDauy.mp4";
+const video = "https://qu.ax/DDauy.mp4";
 
 const menuHeader = `
 
