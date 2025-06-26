f202c4f14deabf136e2d56420d5e935476402e8d
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-06-26 12:46:17 -0700

    Actualizar info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -32,7 +32,7 @@ let handler = async (m, { conn, isRowner }) => {
 
 handler.help = ['status'];
 handler.tags = ['info'];
-handler.command = /^(estado|info|estate|state|stado|stats)$/i;
+handler.command = ['status', 'estado'];
 handler.register = true;
 export default handler;
 
